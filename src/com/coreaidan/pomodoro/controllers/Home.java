0add114f161913059f318e1c522237f402df16ec
InfamousWolf <AidanMelendez@gmail.com>
2017-10-06 13:50:36 -0400

    Properties and Bindings
    created a direct bind for the timer label to handle all event firing for
    two way binding

--- src/com/coreaidan/pomodoro/controllers/Home.java
+++ src/com/coreaidan/pomodoro/controllers/Home.java
@@ -2,6 +2,8 @@ package com.coreaidan.pomodoro.controllers;
 
 import com.coreaidan.pomodoro.model.AttemptKind;
 import com.coreaidan.pomodoro.model.Attempt;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -13,11 +15,37 @@ public class Home {
 
     private Attempt currentAttemtp;
 
+    private StringProperty timerText;
+
+    public Home (){
+        timerText = new SimpleStringProperty();
+        setTimerText(3000);
+    }
+
+    public String getTimerText() {
+        return timerText.get();
+    }
+
+    public StringProperty timerTextProperty() {
+        return timerText;
+    }
+
+    public void setTimerText(String timerText) {
+        this.timerText.set(timerText);
+    }
+
+    public void setTimerText(int remainingSeconds){
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        setTimerText(String.format("%02d:%02d", minutes, seconds));
+    }
+
     private void prepareAttempt(AttemptKind kind){
         clearAttemptStyles();
         currentAttemtp = new Attempt(kind, "");
         addAttemptStyle(kind);
         title.setText(kind.getDisplayName());
+        setTimerText(currentAttemtp.getRemainingSeconds());
     }
 
     private void addAttemptStyle(AttemptKind kind) {
